dc1a1ad8848b402f8e98d472f32ada2736e25d71
Nexus <55708705+Nexus011@users.noreply.github.com>
2020-11-27 11:57:36 +0100

    Modifications backend + frontend au footer

--- footer.php
+++ footer.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta lang="fr">
+  <link rel="stylesheet" type="text/css" href="global.css">
   <link rel="stylesheet" type="text/css" href="/fontawesome-free-5.15.1-web/css/all.css">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
@@ -10,6 +11,19 @@
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 </head>
 <body>
-  <footer class="mx-auto" style="width: 150px; margin-top: 50px;"><p><i class="far fa-copyright"></i> all rights reserved.</p></footer>
+  <footer>
+    <hr style="margin-top: 60px; background-color: #ecf0f1;">
+    <div class="mx-auto" style="width: 150px; margin-top: 10px;"><p><i class="far fa-copyright"></i> all rights reserved.</p></div>
+    <div class="row" style="margin-top: 50px !important;">
+      <div class="col-md-1"></div>
+      <div>
+        <h5>Navigation</h5>
+        <ul class="list-unstyled text-small">
+          <?= nav_menu(); ?>
+        </ul>
+      </div>
+    </div>
+  </footer>
 </body>
 </html>
+
